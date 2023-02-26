e1e34a50df8dfb7340e01c2cb8fd7649dc85e6d1
Bharti Kumari <bhartik021@gmail.com>
2023-02-26 19:10:48 +0530

    health tip page done

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ document.querySelectorAll(".nav-links").forEach((link) => {
         link.setAttribute("aria-current", "page");
     }
 });
+
+
+// Wait for the page to finish loading before running the code
+$(document).ready(function() {
+    const tips = [
+      "Stay hydrated by drinking plenty of water throughout the day",
+      "Eat a well-balanced diet rich in fruits, vegetables, and lean proteins",
+      "Take regular prenatal vitamins as recommended by your healthcare provider",
+      "Get plenty of rest and prioritize sleep",
+      "Exercise regularly with low-impact activities like walking or prenatal yoga",
+      "Avoid alcohol, tobacco, and drugs during pregnancy",
+      "Attend all scheduled prenatal appointments with your healthcare provider",
+      "Practice stress-management techniques like meditation or deep breathing",
+      "Educate yourself on the signs and symptoms of preterm labor",
+      "Prepare for labor and delivery by taking childbirth education classes",
+      "Ask for help and support from friends, family, or a mental health professional if needed"
+    ];
+  
+    $("#tip-button").click(function() {
+      const randomIndex = Math.floor(Math.random() * tips.length);
+      $("#tip-text").text(tips[randomIndex]);
+    });
+  });
+  
