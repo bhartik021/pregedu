dbc9f8ae53e48dc7f11f4dfeb44ed922677a842e
Bharti Kumari <bhartik021@gmail.com>
2023-02-26 15:18:07 +0530

    updated

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".nav-link").forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }
+});
