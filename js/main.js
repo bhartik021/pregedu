65c5c8f76cc45ef052717a85377ab06629f348ac
Bharti Kumari <bhartik021@gmail.com>
2023-02-26 15:34:55 +0530

    nav links in landing page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-document.querySelectorAll(".nav-link").forEach((link) => {
+document.querySelectorAll(".nav-links").forEach((link) => {
     if (link.href === window.location.href) {
         link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }else {
+        link.classList.remove("active");
         link.setAttribute("aria-current", "page");
     }
 });
