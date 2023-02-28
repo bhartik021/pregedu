3527ce15b278ad00b2129df59ab09feae85d9efe
Bharti Kumari <bhartik021@gmail.com>
2023-02-28 11:49:34 +0530

    faq page bug fixed

--- js/main.js
+++ js/main.js
@@ -34,23 +34,6 @@ $(document).ready(function () {
 });
 // Random Health Tip Card JavaScript for Random Health Tip Card Page (Inside Explore) Ending
 
-// Accordian JavaScript For FAQ Page Starting
-const accordions = document.querySelectorAll(".accordion");
-
-accordions.forEach((accordion) => {
-  const header = accordion.querySelector(".accordion-header");
-  const body = accordion.querySelector(".accordion-body");
-
-  header.addEventListener("click", () => {
-    accordion.classList.toggle("active");
-    if (accordion.classList.contains("active")) {
-      body.style.maxHeight = body.scrollHeight + "px";
-    } else {
-      body.style.maxHeight = 0;
-    }
-  });
-});
-// Accordian JavaScript For FAQ Page Ending
 
 function sendEmail() {
 
